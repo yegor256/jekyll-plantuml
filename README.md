7fad53c6346782a03eb0bc8cfd868cee2fc7d03f
Yegor Bugayenko <yegor@tpc2.com>
2014-06-24 17:46:50 +0200

    gem is ready

--- README.md
+++ README.md
@@ -1,15 +1,43 @@
-Add `plantuml.rb` to your `_plugins` directory and include 
-diagrams like this:
+Install it first:
+
+```
+gem install jekyll-plantuml
+```
+
+With Jekyll 2, simply add the gem to your `_config.yml` gems list:
+
+```yaml
+gems: ['jekyll-plantuml', ... your other plugins]
+```
+
+Or for previous versions,
+create a plugin file within your Jekyll project's `_plugins` directory:
+
+```ruby
+# _plugins/plantuml-plugin.rb
+require "jekyll-plantuml"
+```
+
+Highly recommend to use Bundler. If you're using it, add this line
+to your `Gemfile`:
+
+```
+gem "jekyll-plantuml"
+```
+
+Then, make sure [PlantUML](http://plantuml.sourceforge.net/download.html)
+is installed on your build machine, and can
+be executed with a simple `plantuml` command.
+
+Now, it's time to create a diagram, in your Jekyll blog page:
 
 ```
 {% plantuml %}
-@startuml
 [First] - [Second]
-@enduml
 {% endplantuml %}
 ```
 
-Don't forget to add `gem require jekyll-plantuml` to your `Gemfile`.
-
-That's it.
+Check [this blog post](http://www.yegor256.com/2014/06/01/aop-aspectj-java-method-logging.html).
+UML sequence diagram in this page is generated with this plugin.
+Blog sources are available in [Github](https://github.com/yegor256/blog).
 
---
+++ lib/plantuml.rb
@@ -0,0 +1,53 @@
+# (The MIT License)
+#
+# Copyright (c) 2014 Yegor Bugayenko
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+require 'digest'
+require 'fileutils'
+
+module Jekyll
+  class PlantumlBlock < Liquid::Block
+    def initialize(tag_name, markup, tokens)
+      super
+    end
+
+    def render(context)
+      site = context.registers[:site]
+      name = Digest::MD5.hexdigest(super)
+      if !File.exists?(File.join(site.dest, "uml/#{name}.svg"))
+        uml = File.join(site.source, "uml/#{name}.uml")
+        FileUtils.mkdir_p(File.dirname(uml))
+        File.open(uml, 'w') { |f|
+          f.write("@startuml\n")
+          f.write(super)
+          f.write("\n@enduml")
+        }
+        system("plantuml -tsvg #{uml}")
+        site.static_files << Jekyll::StaticFile.new(
+          site, site.source, 'uml', "#{name}.svg"
+        )
+      end
+      "<p><img src='/uml/#{name}.svg' alt='UML' style='width:100%;'/></p>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('plantuml', Jekyll::PlantumlBlock)
