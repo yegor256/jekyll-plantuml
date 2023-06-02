389dd967452af23f7b208613f0fb036822cd9ac2
Yegor Bugayenko <yegor256@gmail.com>
2023-06-02 08:20:07 +0800

    #21 year up to 2023

--- lib/jekyll_plantuml.rb
+++ lib/jekyll_plantuml.rb
@@ -2,7 +2,7 @@
 
 # (The MIT License)
 #
-# Copyright (c) 2014-2019 Yegor Bugayenko
+# Copyright (c) 2014-2023 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -27,7 +27,7 @@ require 'fileutils'
 
 # Jekyll main module.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2014-2019 Yegor Bugayenko
+# Copyright:: Copyright (c) 2014-2023 Yegor Bugayenko
 # License:: MIT
 module Jekyll
   # The main class
