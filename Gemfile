d45e46627fefad9a867b76b12febea1f4e1155c0
Yegor Bugayenko <yegor256@gmail.com>
2025-04-24 08:31:53 +0300

    #95 cov configured

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gemspec
 
 gem 'kramdown-parser-gfm', '1.1.0', require: false
 gem 'minitest', '~>5.25', require: false
+gem 'minitest-reporters', '~>1.7', require: false
 gem 'rake', '~>13.2', require: false
 gem 'rubocop', '~>1.64', require: false
 gem 'rubocop-minitest', '>0', require: false
@@ -15,3 +16,4 @@ gem 'rubocop-performance', '>0', require: false
 gem 'rubocop-rake', '>0', require: false
 gem 'rubocop-rspec', '~>3.3', require: false
 gem 'simplecov', '~>0.22', require: false
+gem 'simplecov-cobertura', '~>2.1', require: false
--- test/test__helper.rb
+++ test/test__helper.rb
@@ -6,6 +6,26 @@
 $stdout.sync = true
 
 require 'simplecov'
-SimpleCov.start
+require 'simplecov-cobertura'
+unless SimpleCov.running
+  SimpleCov.command_name('test')
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      SimpleCov::Formatter::CoberturaFormatter
+    ]
+  )
+  SimpleCov.minimum_coverage 80
+  SimpleCov.minimum_coverage_by_file 80
+  SimpleCov.start do
+    add_filter 'test/'
+    add_filter 'vendor/'
+    add_filter 'target/'
+    track_files 'lib/**/*.rb'
+    track_files '*.rb'
+  end
+end
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
